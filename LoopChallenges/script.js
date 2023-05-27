1622d593bf34a1552006b9503bf8510e3d15236d
Chase Malarkey <127543574+chasemalarkey@users.noreply.github.com>
2023-05-27 04:37:21 -0400

    Add files via upload
    Loop challenges!

---
+++ LoopChallenges/script.js
@@ -0,0 +1,54 @@
+// Prints odds 1-20
+
+// for(let i = 0; i<=20; i++){
+//     if(i % 2 == 1){
+//         console.log(i)
+//     }
+// }
+
+// Decreasing Multiples of 3
+
+// for(let i = 100; i >= 0; i--){
+//     if(i % 3 == 0 && i != 0){
+//         console.log(i)
+//     }
+// }
+
+// Print the sequence
+// for(let i = -3.5; i <= 4; i+= 0.5){
+//     if(i == -3.5){
+//         console.log(i)
+//     }
+//     else if(i == -2){
+//         console.log(i)
+//     }
+//     else if(i == -0.5){
+//         console.log(i)
+//     }
+//     else if(i == 1){
+//         console.log(i)
+//     }
+//     else if(i == 2.5){
+//         console.log(i)
+//     }
+//     else if(i == 4){
+//         console.log(i)
+//     }
+// }
+
+//Sigma
+// let sum = 0;
+// for(let i = 1; i <=100; i++){
+//     sum += i;
+// }
+// console.log(sum);
+
+//Factorial
+
+let product = 1;
+
+for(let i = 1; i <=12; i++){
+    product *= i;
+}
+
+console.log(product);
