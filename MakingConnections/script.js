002bd6af8b4d493b799e746497cc1a41f4952a90
Chase Malarkey <127543574+chasemalarkey@users.noreply.github.com>
2023-06-04 00:14:25 -0400

    Add files via upload

---
+++ MakingConnections/script.js
@@ -0,0 +1,12 @@
+function removeTodd(){
+    var element = document.getElementById("toddR");
+    element.remove();
+}
+function removePhil(){
+    var element = document.getElementById("philR");
+    element.remove();
+}
+function changeName(){
+    var element = document.getElementById("nameOfNew");
+    element.innerText = "Jane Doe";
+}
